e60123a9e6e2781678171828960cb098bb69a53b
Jorge Cayetano <cayetano.jorge@outlook.com>
2024-12-09 16:15:41 +0100

    first commit

---
+++ src/main/java/com/njdealsandclicks/NjdealsandclicksApplication.java
@@ -0,0 +1,13 @@
+package com.njdealsandclicks;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NjdealsandclicksApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NjdealsandclicksApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/njdealsandclicks/NjdealsandclicksApplicationTests.java
@@ -0,0 +1,13 @@
+package com.njdealsandclicks;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NjdealsandclicksApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
